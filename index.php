36c771f6f03b137fc355ae6d422fefc61d691308
Eldar Salimov <salimov.kzn@gmail.com>
2019-10-08 22:48:00 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+//Блок 1
+//Основы синтаксиса
+//Примеры решения задач
+//Задание 1
+echo "<h4>Примеры решения задач </br>Lesson 1 </h4>";
+$var = "hello";
+echo $var[0];
+echo $var[1];
+echo $var[4];
+
+//Задание 2
+echo "<h4>Lesson 2 </h4>";
+$second = 60;
+$hours = 2;
+$res = $hours * ($second * 60);
+echo "Количество секунд в $hours часах равна $res";
+
+//Задание 3
+echo "<h4>Lesson 3 </h4>";
+$var = 1;
+$var += 12;
+$var -= 14;
+$var *= 5;
+$var /= 7;
+$var++;
+$var--;
+echo $var;
+
+//Задачи для решения
+//echo "</br></br>";
+echo "<h4>Работа с переменными</h4>";
+$a = 3;
+echo "Переменная а равна: $a</br></br>";
+
+$a = 10;
+$b = 2;
+$resSum = $a + $b;
+$resRazn = $a - $b;
+$resProizv = $a * $b;
+$resDelen = $a / $b;
+echo "Переменная а равна: $a</br>Переменная b равна: $b</br>Их сумма: $resSum</br>Их разность: $resRazn</br>
+Их произведение: $resProizv</br>Их частное: $resDelen";
+
+echo "</br></br>";
+$c = 15;
+$d = 2;
+$result = $c + $d;
+echo $result;
+echo "</br></br>";
+
+$a=10;
+$b=2;
+$c=5;
+echo $a + $b + $c;
+echo "</br></br>";
+
+$a=17;
+$b=2;
+$c= $a - $b;
+$d = 7;
+$result = $c + $d;
+echo $result;
+echo "</br></br>";
+
+$text = 'Я';
+$text = $text.' хочу';
+$text = $text.' знать';
+$text = $text.' PHP!';
+echo $text;
+?>
+
+<?php
+
+//Блок 1
+//Основы работы с массивами на PHP
+echo "</br></br>";
+
+?>
+
+
+
+
+
